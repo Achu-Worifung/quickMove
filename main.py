d64a682dd759916266b4942b4081c5afb63c5480
Clinton's MiniPC <achuworifung@gmail.com>
2024-12-13 20:29:51 -0700

    set up for quick hsp project

---
+++ main.py
@@ -0,0 +1,14 @@
+# build using pip install pynput
+
+from pynput.mouse import Button, Controller
+
+mouse = Controller()
+
+def on_click(x, y)
+
+if __name__ == '__main__':
+    while(True):
+        # works for both two screens 
+        print(f'the mouse position is {mouse.position}')
+    
+    
