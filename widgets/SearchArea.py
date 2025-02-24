ce2a3d9e428c8fd546e8c0ae82857d49f3299803
Clinton's MiniPC <achuworifung@gmail.com>
2025-02-24 10:09:08 -0700

    finished show functionality

--- widgets/SearchArea.py
+++ widgets/SearchArea.py
@@ -3,6 +3,14 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 from util import Message as msg
 from PyQt5.QtCore import QSettings
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QBrush, QColor
+from PyQt5.QtWidgets import (QApplication, QPushButton, QMainWindow,
+                           QWidget, QVBoxLayout)
+from PyQt5.QtCore import QRect
+
+overlays = []
 class SearchArea(QWidget):
     def __init__(self, page_widget):
         super().__init__()
@@ -19,7 +27,7 @@ class SearchArea(QWidget):
         
        
         
-        self.show_button.clicked.connect(self.show_search)
+        self.show_button.clicked.connect(self.show_overlay)
         self.configure.clicked.connect(self.configureSearch)
         
         self.updateLable()
@@ -31,9 +39,21 @@ class SearchArea(QWidget):
         else:
             self.label.setText("No search area configured")
         
-    def show_search(self):
-        print('showing search')
-        pass
+
+    def show_overlay(self):
+        global overlays
+        overlays = []
+        # Clear any existing overlays
+        for overlay in overlays:
+            overlay.close()
+            overlays.clear()
+        
+        # Create an overlay for each screen
+        for screen in QApplication.screens():
+            overlay = ScreenOverlay(screen)
+            overlays.append(overlay)
+            overlay.show()
+
     def configureSearch(self):
         from util.event_tracker import EventTracker
         msg.informationBox(self, 'Search Area', "Click Ok to get started. Click on the top-left corner, then the bottom-right corner to set the area. Press 'ESC' to stop.")
@@ -51,3 +71,38 @@ class SearchArea(QWidget):
         self.event_tracker_thread = EventTracker()
         self.event_tracker_thread.tracking_finished.connect(setup_search_area)
         self.event_tracker_thread.create_new_automaton()
+        
+class ScreenOverlay(QWidget):
+    def __init__(self, screen):
+        super().__init__()
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        
+        # Set the geometry to cover the entire screen
+        self.setGeometry(screen.geometry())
+        self.geometry = screen.geometry()
+        
+        self.settings = QSettings("MyApp", "AutomataSimulator")
+        
+        self.search_area = self.settings.value('search_area')
+        
+        self.square_rect = QRect(self.search_area[0], self.search_area[1], self.search_area[2] - self.search_area[0], self.search_area[3] - self.search_area[1])  
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setBrush(QBrush(QColor(0, 0, 0, 150)))  
+        painter.setPen(Qt.NoPen)
+        painter.drawRect(self.rect())
+        
+        
+        if(self.geometry.x() <= self.search_area[0] <= self.geometry.x() + self.geometry.width() and self.geometry.y() <= self.search_area[1] <= self.geometry.y() + self.geometry.height()):
+            # Draw the highlighted square
+            painter.setBrush(QBrush(QColor(255, 255, 255, 100))) 
+            painter.setPen(Qt.NoPen)
+            painter.drawRect(self.square_rect)
+    
+    def mousePressEvent(self, event):
+        global overlays
+        for overlay in overlays:
+            overlay.close()
+            # overlay.clear()
