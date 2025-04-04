3ae07b31618138b2d40786887b25c6ab8fadb45c
worifung achu <achuclinton47@gmail.com>
2025-04-03 17:46:56 -0700

    feat: moved model variables to app settings -stored the variables in the main using QSettings -retrieved the variables from the settings in the whisperer
    Former-commit-id: 430acceb885661d62ce425dc9626582b31903dea
    Former-commit-id: 9f4c01195321efe51649966e6128eb51519a5f37

--- util/pyaudioandWhisper.py
+++ util/pyaudioandWhisper.py
@@ -11,47 +11,57 @@ import tempfile
 settings = QSettings("MyApp", "AutomataSimulator")
     
 def get_energy_threshold(audio_data, threshold_value=0.05):
+    # print('here is the energy threshold', threshold_value)
     audio_data = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0  # Normalize
     energy = np.sum(audio_data ** 2) / len(audio_data)  # Calculate energy
+    # print('here is the energy', energy)
     return energy > threshold_value
     
 def run_transcription(recording_page):
     
-    beam_size = settings.value('beam')
-    best_of = settings.value('best')
-    temperature = settings.value('temperature')
+    # --------------------loading all the variables from settings------------------
+    
+    beam_size = int(settings.value('beam'))
+    best_of = int(settings.value('best'))
+    temperature = float(settings.value('temperature'))
     language = settings.value('language')
-    vad_filter = settings.value('vad_filter')
-    energy_threshold = settings.value('energy_threshold')
-    vad_parameters = settings.value('vad_parameters')
-    model_size = settings.value('model_size')
-    cpu_cores = settings.value('cpu_cores')
+    # vad_filter = settings.value('vad_filter')
+    energy_threshold = float(settings.value('energy_threshold'))
+    # vad_parameters = settings.value('vad_parameters')
+    model_size = settings.value('model').lower()
+    cpu_cores = int(settings.value('cpu_cores'))
     processing = settings.value('processing')
-    CHANNELS = settings.value('channel')
-    RATE = settings.value('rate')  # 16kHz sample rate for Whisper
-    CHUNK = settings.value('chunks')
-    silence_length = settings.value('silence')  # Minimum silence duration for VAD in seconds
-    min_record_len = settings.value('minlen')  # Minimum recording length in seconds
-    max_record_len = settings.value('maxlen')  # Maximum recording length in seconds
+    CHANNELS = int(settings.value('channel'))
+    RATE = int(settings.value('rate'))  # 16kHz sample rate for Whisper
+    CHUNK = int(settings.value('chunks'))
+    silence_length = float( settings.value('silence'))  # Minimum silence duration for VAD in seconds
+    min_record_len = int(settings.value('minlen'))  # Minimum recording length in seconds
+    
+    max_record_len = int(settings.value('maxlen'))  # Maximum recording length in seconds
+    computation_type = "float16" if processing == "GPU" else "int8"
     
     line_edit = recording_page.lineEdit
-    print('here is the line edit', line_edit)
-    print('here is the passed page', recording_page)
-    # Setting up whisper model
     
+    # -----------------------------loging to debug--------------------------
+    
+    # print('here is the line edit', line_edit)
+    # print('here is the passed page', recording_page)
+    
+    # Setting up whisper model
+    print(f"processing {processing} model {model_size} computational type {computation_type} cpu cores {cpu_cores}" )
+    model_size = model_size.lower()
     model = WhisperModel(
         model_size,
         device=processing,
-        compute_type='int8',
+        compute_type=computation_type,
         num_workers=cpu_cores,
         cpu_threads=8,
-        download_root='./models'
+        download_root='./models' #where dowunloaded models are stored
     )
 
     # Recording settings
     FORMAT = pyaudio.paInt16
 
-    energy_threshold = 0.001  # Energy threshold for VAD
 
 
     # Initialize PyAudio
@@ -124,16 +134,16 @@ def run_transcription(recording_page):
                     print(f"Transcribing segment {counter}...")
                     segments, _ = model.transcribe(
                         temp_filename,
-                        beam_size=5,
-                        best_of=1,
-                        temperature=0.0,
-                        language='en',
+                        beam_size=beam_size,
+                        best_of=best_of,
+                        temperature=temperature,
+                        language=language,
                         vad_filter=True,
                         vad_parameters=dict(min_silence_duration_ms=500),
                         word_timestamps=False
                     )
                     
-                    print(f"Transcription for segment {counter}:")
+                    # print(f"Transcription for segment {counter}:")
                     segment_text = ""
                     for segment in segments:
                         segment_text += segment.text + " "
@@ -147,7 +157,7 @@ def run_transcription(recording_page):
                     counter += 1
                     
                 except Exception as e:
-                    # print(f"Error processing audio: {e}")
+                   
                     pass
                 finally:
                     # Clean up the temporary file
