d89523869a02d7f2ae42fb435246b00ca7e9bab7
Achu Worifung <achuworifung@gmail.com>
2025-11-19 08:46:15 -0700

    fixed inacurate search area configuration on laptop due to scalling

--- util/configureSearchArea.py
+++ util/configureSearchArea.py
@@ -1,5 +1,6 @@
 from pynput import  mouse
 from PyQt5.QtCore import QObject, pyqtSignal
+import ctypes
 
 class SearchArea(QObject):
     tracking_finished = pyqtSignal(tuple)  
@@ -17,11 +18,16 @@ class SearchArea(QObject):
         self.listener.join()
     def on_click(self, x, y, button, pressed):
         if pressed:
+            # Get the scaling factor
+            scaling_factor = ctypes.windll.shcore.GetScaleFactorForDevice(0) / 100.0
+            adjusted_x = int(x / scaling_factor)
+            adjusted_y = int(y / scaling_factor)
+
             if self.x1 is None and self.y1 is None:
-                self.x1, self.y1 = x, y
+                self.x1, self.y1 = adjusted_x, adjusted_y
                 print(f"First corner set at: ({self.x1}, {self.y1})")
             else:
-                self.x2, self.y2 = x, y
+                self.x2, self.y2 = adjusted_x, adjusted_y
                 print(f"Second corner set at: ({self.x2}, {self.y2})")
                 # Stop listener
                 self.tracking_finished.emit((self.x1, self.y1, self.x2, self.y2))
